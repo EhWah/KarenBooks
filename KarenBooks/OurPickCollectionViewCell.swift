c15257de2ac644119a5f8bc1dfc96ae153ef933e
ehwah <soewah1996@gmail.com>
2020-09-08 16:48:26 -0400

    json fetching for our pick

--- KarenBooks/OurPickCollectionViewCell.swift
+++ KarenBooks/OurPickCollectionViewCell.swift
@@ -13,4 +13,22 @@ class OurPickCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var bookCover: UIImageView!
     @IBOutlet weak var booktitle: UILabel!
   
+    func configure(book: Book) {
+        
+        booktitle.text = book.bookTitleEnglish
+    
+        if let url = URL(string: book.bookCoverURL) {
+            bookCover.af.setImage(withURL: url)
+        } else {
+            bookCover.image = UIImage(named: "notAvailable")
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        bookCover.af.cancelImageRequest()
+        bookCover.image = nil
+    }
+    
+   
 }
--- KarenBooks/OurPickViewController.swift
+++ KarenBooks/OurPickViewController.swift
@@ -7,27 +7,49 @@
 //
 
 import UIKit
+import ProgressHUD
+import Alamofire
 
 class OurPickViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    var books = [Book]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         collectionView.delegate = self
         collectionView.dataSource = self
+        requestJson()
+    }
+    
+    func requestJson() {
+        
+        AF.request("https://raw.githubusercontent.com/EhWah/Karen-Library/master/featuresData.json").response { (response) in
+            if let JSON = response.data {
+                self.parse(json: JSON)
+            }
+        }
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        if let json = try? decoder.decode(Books.self, from: json) {
+            books.append(contentsOf: json.books)
+        }
+        collectionView.reloadData()
     }
 
 }
 
 extension OurPickViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return books.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ourPickCell", for: indexPath) as! OurPickCollectionViewCell
+        cell.configure(book: books[indexPath.row])
         return cell
     }
     
